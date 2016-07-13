95f16d25a26253f62d93cf1f765056a5b89c4e15
mastermalone <michael.malone@aofl.com>
2016-07-12 18:36:24 -0700

    Creating service-worker and supporting service.

--- js/service-worker-service/0.1/index-impl.js
+++ js/service-worker-service/0.1/index-impl.js
@@ -11,6 +11,13 @@ define([], function () {
       register: function register() {
         if ('serviceWorker' in navigator) {
           console.log('WE HAVE SERVICE WORKERS');
+          navigator.serviceWorker.register('/service-worker/0.1/index.js', {
+            scope: '/'
+          }).then(function (reg) {
+            console.log('Registration succeeded, Scope is ', reg.scope);
+          }).catch(function (error) {
+            console.log('There was an error during regsitration:', error);
+          });
         }
       }
     };
